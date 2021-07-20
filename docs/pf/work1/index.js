b3458518949bf4949c3a955461c7a573865d7918
manyrice <alsgml1966@naver.com>
2021-07-20 15:45:41 +0900

    오버뷰 gsap 완료

--- docs/pf/work1/index.js
+++ docs/pf/work1/index.js
@@ -1,1 +1,13 @@
-gsap.to(".img1", { x: 300 });
+gsap.to(".img1", {
+  x: 300
+});
+
+gsap.from(".section-2 > div:last-child > img", {
+  ease: Expo.easeIn,
+  opacity: 0,
+  x: -300,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.section-2'
+  }
+});
