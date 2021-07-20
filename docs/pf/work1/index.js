51dd9ddbad4612df51cad4496f81a3c82ffbe26c
schim613 <uximin@naver.com>
2021-07-20 15:42:37 +0900

    이상해요...

--- docs/pf/work1/index.js
+++ docs/pf/work1/index.js
@@ -1,1 +1,19 @@
 gsap.to(".box-1", { x: 300 });
+
+
+// img-3 : 로고 아이템
+gsap.from('.img-3', {
+  ease:Power2.easeIn,
+  opacity:0,
+  y:500,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.section-3',
+    markers:true,
+    start:'0%, 50%',
+    end:'80%, 30%',
+    // 내려가면서 만났을 때, 내려가면서 지나쳤을 때, 올라가면서 만났을 때, 올라가면서 지나쳤을 때
+    // onEnter, onLeave, onEnterBack and on LeaveBack
+    toggleActions:'restart none none none '
+  }
+})
